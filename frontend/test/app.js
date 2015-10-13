fc61c6f3e81470790d92316decae5448a880d27e
greg <ku392000@gmail.com>
2015-10-13 15:16:33 +0800

    [what] construct test environment [how]  test angular with karma & jasmine

---
+++ frontend/test/app.js
@@ -0,0 +1,12 @@
+describe('main', function() {
+    beforeEach(module('dispatchApp'))
+
+    beforeEach(inject(function($controller, $rootScope, $modal) {
+        var scope = $rootScope.$new();
+        var ctrl = $controller('dispatchCtrl', {$scope: scope, $modal: $modal});
+    }));
+
+    it('should select English', function() {
+        expect(3).toBe(3);
+    });
+});
