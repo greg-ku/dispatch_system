43050b445758cf317644090486c56d435ad1be5d
greg <ku392000@gmail.com>
2015-10-22 16:34:42 +0800

    [what] write a real test for language controller

--- frontend/test/app.js
+++ frontend/test/app.js
@@ -1,12 +1,26 @@
 describe('main', function() {
+    var scope, ctrl, modalInstance;
+ 
     beforeEach(module('dispatchApp'));
 
-    beforeEach(inject(function($controller, $rootScope, $modal) {
-        var scope = $rootScope.$new();
-        var ctrl = $controller('dispatchCtrl', {$scope: scope, $modal: $modal});
+    beforeEach(inject(function($controller, $rootScope, $translate, $http) {
+        scope = $rootScope.$new();
+        modalInstance = {
+            close: jasmine.createSpy('modalInstance.close'),
+            dismiss: jasmine.createSpy('modalInstance.dismiss'),
+            result: {
+                then: jasmine.createSpy('modalInstance.then'),
+            }
+        };
+        ctrl = $controller('languageCtrl', {
+            $scope: scope,
+            $translate: $translate,
+            $modalInstance: modalInstance,
+            $http: $http
+        });
     }));
 
     it('should select English', function() {
-        expect(3).toBe(3);
+        expect(scope.selectedLang.value).toBe('en-US');
     });
 });
